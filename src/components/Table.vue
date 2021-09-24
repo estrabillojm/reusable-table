676ac4afc24075af6df1f68cb4fe405519b9ca86
John Michael Estrabillo <estrabillo.johnmichael@gmail.com>
2021-09-24 13:39:58 +0800

    Pushing to Master Branch

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,29 +1,28 @@
 <template>
-  <table width="100%">
-      <thead>
-          <tr>
-              <th class="border"  v-for="(h, index) in headers" :key="index">{{ h }}</th>
-          </tr>
-      </thead>
-      <tbody>   
-        <tr v-for="(d, index) in data" :key="index">
-            <td v-for="(h, index) in dataParams" :key="index">
-                {{ d[h.toString()] }}
-            </td>
-            <td>
-                <button>Add</button>
-                <button>Edit</button>
-                <button>Delete</button>
-            </td>
-        </tr>   
-      </tbody>
-  </table>
+    <table width="100%">
+        <thead>
+            <tr>
+                <th class="border"  v-for="(h, index) in headers" :key="index">{{ h }}</th>
+            </tr>
+        </thead>
+        <tbody>   
+            <tr v-for="(d, index) in data" :key="index">
+                <td v-for="(h, index) in dataParams" :key="index">
+                    {{ d[h.toString()] }}
+                </td>
+                <td>
+                    <button>Add</button> <!-- SET CONDITIONS IF WHAT DO YOU WANT TO OUTPUT USING V-IF -->
+                    <button>Edit</button> <!-- SET CONDITIONS IF WHAT DO YOU WANT TO OUTPUT USING V-IF -->
+                    <button>Delete</button> <!-- SET CONDITIONS IF WHAT DO YOU WANT TO OUTPUT USING V-IF -->
+                </td>
+            </tr>   
+        </tbody>
+    </table>
 </template>
 <script>
 export default {
     props: {
         headers: { 
-
             required: true,
             type: Array
         },
